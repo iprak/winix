e58fc89fbf90319a1e13de1a747188ecd1c93d73
Indu Prakash <leolite1@gmail.com>
2022-12-21 10:13:32 -0600

    Implemented conflg_flow

--- custom_components/winix/__init__.py
+++ custom_components/winix/__init__.py
@@ -1,53 +1,84 @@
-"""The Winix C545 Air Purifier component."""
+"""The Winix Air Purifier component."""
 
 from __future__ import annotations
 
-from datetime import timedelta
 import logging
 
-from homeassistant.const import CONF_PASSWORD, CONF_SCAN_INTERVAL, CONF_USERNAME
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
-import homeassistant.helpers.config_validation as cv
-import voluptuous as vol
 
+from custom_components.winix.helpers import Helpers
 from custom_components.winix.manager import WinixManager
+from winix import auth
 
-from .const import DOMAIN
+from .const import WINIX_AUTH_RESPONSE, WINIX_DATA_COORDINATOR, WINIX_DOMAIN, WINIX_NAME
 
 _LOGGER = logging.getLogger(__name__)
-MIN_SCAN_INTERVAL = timedelta(seconds=30)
-SUPPORTED_PLATFORMS = ["fan", "sensor"]
+SUPPORTED_PLATFORMS = [Platform.FAN, Platform.SENSOR]
 
-CONFIG_SCHEMA = vol.Schema(
-    {
-        DOMAIN: vol.Schema(
-            {
-                vol.Required(CONF_USERNAME): cv.string,
-                vol.Required(CONF_PASSWORD): cv.string,
-                vol.Optional(
-                    CONF_SCAN_INTERVAL, default=MIN_SCAN_INTERVAL
-                ): cv.time_period,
-            }
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up the Winix component."""
+
+    hass.data.setdefault(WINIX_DOMAIN, {})
+    user_input = entry.data
+
+    auth_response_data = user_input.get(WINIX_AUTH_RESPONSE)
+    auth_response = (
+        auth_response_data
+        if isinstance(auth_response_data, auth.WinixAuthResponse)
+        else auth.WinixAuthResponse(**auth_response_data)
+    )
+
+    if not auth_response:
+        Helpers.send_notification(
+            hass,
+            "async_setup_entry",
+            WINIX_NAME,
+            "No authentication data found. Please reconfigure the integration.",
         )
-    },
-    extra=vol.ALLOW_EXTRA,
-)
+        return False
 
+    # try:
+    #     new_auth_response = await Helpers.async_refresh_auth(hass, auth_response)
 
-async def async_setup(hass: HomeAssistant, config):
-    """Set up the component."""
-    domain_config = config[DOMAIN]
-    scan_interval = domain_config.get(CONF_SCAN_INTERVAL, MIN_SCAN_INTERVAL)
+    #     auth_response.access_token = new_auth_response.access_token
+    #     auth_response.refresh_token = new_auth_response.refresh_token
+    #     auth_response.id_token = new_auth_response.id_token
 
-    if scan_interval < MIN_SCAN_INTERVAL:
-        _LOGGER.info(
-            "scan interval increased to %s from %s",
-            MIN_SCAN_INTERVAL,
-            scan_interval,
+    # except AuthenticationError as err:
+    #     # Raising ConfigEntryAuthFailed will automatically put the config entry in a
+    #     # failure state and start a reauth flow.
+    #     # https://developers.home-assistant.io/docs/integration_setup_failures/
+    #     if err.error_code == "UserNotFoundException":
+    #         raise ConfigEntryAuthFailed from err
+
+    #     _LOGGER.warning("Unable to authenticate", exc_info=True)
+
+    # except Exception as err:  # pylint: disable=broad-except
+    #     _LOGGER.warning("Unable to authenticate", exc_info=True)
+    #     raise ConfigEntryNotReady(
+    #         "Unable to authenticate. Sensi integration is not ready."
+    #     ) from err
+
+    scan_interval = 30
+    manager = WinixManager(hass, auth_response, scan_interval)
+
+    if await manager.async_prepare_devices_wrappers():
+        # await manager.async_setup_updates()
+        await manager.async_config_entry_first_refresh()
+
+        hass.data[WINIX_DOMAIN][entry.entry_id] = {WINIX_DATA_COORDINATOR: manager}
+        await hass.config_entries.async_forward_entry_setups(entry, SUPPORTED_PLATFORMS)
+    else:
+        Helpers.send_notification(
+            hass, "async_setup_entry", WINIX_NAME, "Unable to obtain device data."
         )
-        scan_interval = MIN_SCAN_INTERVAL
-
-    _LOGGER.debug("Creating locator with scan interval of %s", scan_interval)
-    manager = hass.data[DOMAIN] = WinixManager(hass, domain_config, scan_interval)
-    await hass.async_add_executor_job(manager.login)
     return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    # pylint: disable=unused-argument
+    """Unload a config entry."""
+    hass.data.pop(WINIX_DOMAIN)
